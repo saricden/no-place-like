5db7638e896341ca8c421e29890b086b288220d1
Kirk M <kirk@saricden.com>
2019-02-21 22:01:17 -0800

    #30: Adds setDepth calls to manage layering.

--- src/scenes/levels/base/Level.js
+++ src/scenes/levels/base/Level.js
@@ -21,29 +21,33 @@ class Level extends Scene {
       enemies
     });
 
-    // // Add our maaaaaaap!
+    // Add our maaaaaaap!
     this.map = this.make.tilemap({ key: tilemapKey, tileWidth: 100, tileHeight: 100 });
     const tileset = this.map.addTilesetImage(tilesetName, tilesetImage);
     
-    // // Map layers
+    // Map layers
     this.behindLayer = this.map.createStaticLayer('behind-mc', tileset, 0, 0);    
     this.aboveLayer = this.map.createStaticLayer('above-mc', tileset, 0, 0);
     this.solidLayer = this.map.createStaticLayer('solid', tileset, 0, 0);
 
-
-    // // Map v MC collisions
+    // Map v MC collisions
     for (let i = 0; i < collisionTiles.length; i++) {
       const tiles = collisionTiles[i];
       this.solidLayer.setCollisionBetween(tiles[0], tiles[1]);
     }
 
+    // MC vs enemies & solidLayer collisions
     this.physics.add.collider(this.mc, this.solidLayer);
-    // this.enemies.list.forEach((enemy) => {
-    //   this.physics.add.collider(enemy, this.solidLayer);
-    // });
     this.physics.add.collider(this.enemies, this.solidLayer);
 
-    // // Set camera follow
+    // Setup our layering
+    this.behindLayer.setDepth(-1);
+    this.aboveLayer.setDepth(1);
+    this.solidLayer.setDepth(0);
+    this.mc.setDepth(0);
+    this.enemies.setDepth(0);
+
+    // Set camera follow
     this.cameras.main.startFollow(this.mc);
   }
 
