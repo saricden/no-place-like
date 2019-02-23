83606252b9ec109e98c6fe4fedb4ee4a20074e44
Kirk M <kirk@saricden.com>
2019-02-22 23:13:48 -0800

    #36: Abstracts base enemy logic into it's own class.

--- src/sprites/npc/bad/JumperBot.js
+++ src/sprites/npc/bad/JumperBot.js
@@ -1,42 +1,20 @@
-import {GameObjects} from 'phaser';
+import Enemy from '../../base/Enemy';
 
-const {Sprite} = GameObjects;
-
-class JumperBot extends Sprite {
+class JumperBot extends Enemy {
   constructor(config) {
-    super(config.scene, config.x, config.y, config.key);
-
-    // Add self to scene's physics
-    config.scene.physics.world.enable(this);
-    config.scene.add.existing(this);
-
-    this.setTexture('jump-blaster');
+    super({
+      ...config,
+      texture: 'jump-blaster',
+      maxHP: 50
+    });
 
     // Config
     this.maxSpeed = 100;
     this.jumpHeight = 600;
-    this.maxHP = 50;
-    this.hp = this.maxHP;
 
     // Setup physics properties
     this.body.setSize(939, 848);
     this.setScale(0.1);
-  }
-
-  damageOrKill(damage) {
-    this.hp -= damage;
-
-    if (this.isDead()) {
-      this.setActive(false);
-      this.setVisible(false);
-      return true;
-    }
-
-    return false;
-  }
-
-  isDead() {
-    return (this.hp <= 0);
   }
 
   jump(coinToss) {
