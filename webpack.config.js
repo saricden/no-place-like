b6babc93d11339688e4f7662fce210da546baec2
Kirk M <kirk@saricden.com>
2019-03-07 22:56:46 -0800

    #71: Moves functions so that it'll end up in the /dist dir.

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,9 @@ module.exports = {
     }),
     new CopyWebpackPlugin([
       { from: './src/assets', to: 'assets' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './src/functions', to: 'functions' }
     ])
   ]
 };
