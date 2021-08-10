360f66249ac3d815e0023ee182e8664c748e8454
VocalFan <45863583+Mou-Ikkai@users.noreply.github.com>
2021-08-10 12:28:53 -0400

    Create webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -43,9 +43,8 @@ module.exports = {
           { from: './src/assets', to: 'assets' }
         ]
       }
-    ),
+    ],
   devServer: {
     compress: true,
     disableHostCheck: true,   // That solved it
-  }
-};
+   }
