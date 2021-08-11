1d970662e41abe575132c0af1ff2009df79a2a96
VocalFan <45863583+Mou-Ikkai@users.noreply.github.com>
2021-08-11 12:36:59 -0400

    Update Basic.tsx

--- src/assets/images/tilemaps/Basic.tsx
+++ src/assets/images/tilemaps/Basic.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="Basic" tilewidth="100" tileheight="100" tilecount="35" columns="7">
+<tileset version="1.2" tiledversion="1.2.2" name="Basic" tilewidth="98" tileheight="98" tilecount="35" columns="7">
  <image source="basic.png" width="700" height="500"/>
 </tileset>
